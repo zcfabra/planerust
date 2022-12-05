c201b8d3d264fbe6f6418a5fd71f0f085a8fc073
zcfabra <christianwgnam@gmail.com>
2022-12-05 11:22:53 -0500

    setup

--- img2msg/chunk.rs
+++ img2msg/chunk.rs
@@ -1,13 +1,56 @@
-mod chunk {
+pub mod chunk {
+    use std::{fmt, str::FromStr, io::Read};
+
     use crate::Error;
-
-
 
     pub struct ChunkType {
         length: u32 ,
         chunk_type: u32,
         chunk_data: Vec<u8>,
         crc: u32,
+    }
+
+    impl ChunkType{
+        pub fn bytes(&self)->[u8; 4]{
+            let out= &self.chunk_data;
+            let mut arr = [0u8; 4];
+
+            for (ix, each) in out.iter().enumerate(){
+                arr[ix] = *each;
+            }
+
+            return arr;
+    
+        }
+
+        pub fn is_valid(&self)->bool{
+            let valid_bit = self.chunk_data[1];
+            return valid_bit == 1;
+
+        }
+    }
+
+    impl FromStr for ChunkType{
+        type Err = Error;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let vec = s.as_bytes().to_vec();
+            return Ok(ChunkType { length: vec.len() as u32, chunk_type: 2, chunk_data: vec, crc: 1 });
+        }
+    }
+
+    impl fmt::Display for ChunkType{
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f,"{:?}", self.chunk_data)
+        }
+    }
+
+    impl PartialEq for ChunkType{
+        fn eq(&self, other: &ChunkType)->bool{
+            return self.chunk_data == other.chunk_data;
+        }
+    }
+    impl Eq for ChunkType{
+
     }
 
     impl TryFrom<[u8; 4]> for ChunkType{
--- img2msg/main.rs
+++ img2msg/main.rs
@@ -1,14 +1,15 @@
-mod args;
+// mod args;
 mod chunk;
-mod chunk_type;
-mod commands;
-mod png;
+// mod chunk_type;
+// mod commands;
+// mod png;
+
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
-
 fn main()->Result<()>{
 
-    todo!()
-
+   let chunk = chunk::chunk::ChunkType::try_from([1,2,3,4]).unwrap();
+   println!("{}", chunk);
+   Ok(())
 }
